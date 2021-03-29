c028c38d38178a1200df11471f14b695401ecf74
anderrf <ander_rochafarias@live.com>
2021-03-28 23:07:53 -0300

    Card de adição e grid da lista

--- resources/views/layout/item/list.blade.php
+++ resources/views/layout/item/list.blade.php
@@ -23,13 +23,20 @@
             </aside>
         </div>
         <div class="col-md-10">
-            <div class="card">
-                <img src="https://images.pexels.com/photos/4040600/pexels-photo-4040600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="card-img-top" alt="Quartzo">
-                <div class="card-body">
-                    <h3 class="card-title">Quartzo</h3>
-                    <p class="card-text">2021</p>
-                    <a href="#" class="btn btn-danger">
-                        <i class="bi bi-trash-fill"></i>
+            <div class="container" id="list-grid">
+                <div class="card">
+                    <img src="https://images.pexels.com/photos/4040600/pexels-photo-4040600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="card-img-top" alt="Quartzo">
+                    <div class="card-body">
+                        <h3 class="card-title">Quartzo</h3>
+                        <p class="card-text text-muted">2021</p>
+                        <a href="#" class="btn btn-danger">
+                            <i class="bi bi-trash-fill"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card add-card">
+                    <a href="#">
+                        <i class="bi bi-plus"></i>
                     </a>
                 </div>
             </div>
